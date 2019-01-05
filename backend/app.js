9a00c6d9a3cb8fb17b48c4de15dd5b12e9d28693
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-01-05 14:54:52 +0100

    mongoose installed

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphQlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
+const mongoose = require('')
 
 const app = express()
 
 app.use(bodyParser.json())
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            tasks: [String!]!
+            userTasks: [String!]!
+
+        }
+        type RootMutation {
+            createTask(title: String): String
+            createUserTask(status: String): String
+        }
+
+        schema {
+            query: RootQuery 
+            mutation: RootMutation
+        }
+        `),
+    rootValue: {
+        tasks: () => {
+            return ['test1', 'test2']
+        },
+        createTask: (args) => {
+            const taskTitle = args.title;
+            return taskTitle;
+        }
+    },
+    graphiql: true
+    })
+)
 
 app.listen(3000)
