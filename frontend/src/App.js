79df01365dbedeab08f6a3d5bfafc51ca4df2757
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-01-07 22:10:28 +0100

    empty auth, dashboard pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        test
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect from='/' to='/auth' exact></Redirect>
+          <Route path='/auth' component={Auth}></Route>
+          <Route path='/Dashboard' component={Dashboard}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
