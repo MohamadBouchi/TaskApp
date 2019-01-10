208626888a425a0bd43a94c3afc67be98462b770
Mohamed Bouchi <mohamedbouchi@Mohameds-iMac.local>
2019-01-10 21:01:07 +0100

    enable cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,15 @@ const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 app.use(isAuth);
 app.use('/graphql', graphQlHttp({
     schema: graphQlSchema,
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Auth from './pages/Auth';
 import Dashboard from './pages/Dashboard';
+import NavBar from './components/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from='/' to='/auth' exact></Redirect>
-          <Route path='/auth' component={Auth}></Route>
-          <Route path='/Dashboard' component={Dashboard}></Route>
-        </Switch>
+        <React.Fragment>
+          <NavBar></NavBar>
+          <main>
+            <Switch>
+              <Redirect from='/' to='/auth' exact></Redirect>
+              <Route path='/auth' component={Auth}></Route>
+              <Route path='/Dashboard' component={Dashboard}></Route>
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
